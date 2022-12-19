7e6a56ef9f9f8961b92401a04d61efb670dec424
hashem78 <hashem.olayano@gmail.com>
2022-12-19 22:54:07 +0300

    Remove task function from ThreadArgs

--- src/include/threadArgs.h
+++ src/include/threadArgs.h
@@ -1,11 +1,10 @@
 #pragma  once
 typedef struct ThreadArgs {
-    void (*taskFunction)(void* param);
+
     unsigned char* image;
     unsigned char** partialConvolvedImage;
     int imgRows;
     int rowStart;
     int rowEnd;
     int imgColumns;
-
 } ThreadArgs;
